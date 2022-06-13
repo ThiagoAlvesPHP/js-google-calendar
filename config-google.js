69354eb7989706135d19aa8f4ec3d25c06168c89
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-13 15:22:23 -0300

    google calendar com js

--- config-google.js
+++ config-google.js
@@ -1,6 +1,5 @@
 const CLIENT_ID = '1089260019866-1khls7ucdp5bs8h14l8j8dio01kcjs0k.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyB9L1W-LYF1csM9GqcTFxWZJex8eZ_7fnw';
-
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 const SCOPES = 'https://www.googleapis.com/auth/calendar.events';
 
